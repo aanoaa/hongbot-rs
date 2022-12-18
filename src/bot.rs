f26321941e225921ac73f327ae0b959f1971eb31
Hyungsuk Hong <aanoaa@gmail.com>
2022-12-18 22:52:14 +0900

    impl hear, respond, reply, send for bot

--- src/bot.rs
+++ src/bot.rs
@@ -1,23 +1,72 @@
 use std::{
-    io::{stdout, Write},
-    sync::mpsc::channel,
+    collections::HashMap,
+    str::FromStr,
+    sync::mpsc::{channel, Sender},
     thread::JoinHandle,
 };
 
-use crate::server::{Empty, Server};
+use regex::Regex;
+
+use crate::server::{Server, Shell};
+
+pub enum ServerType {
+    Shell,
+}
+
+type Callback = dyn Fn(&Bot, &str, &str, &str);
 
 pub struct Bot {
     pub name: String,
-    // server params...
+    pub server_type: ServerType,
+    reaction: HashMap<String, Box<Callback>>,
+    resp: HashMap<String, Box<Callback>>,
     running: bool,
+    sender: Option<Sender<(String, String, String)>>,
 }
 
-// trait 으로 interface 명확하게 하자
 impl Bot {
-    pub fn new(name: &str) -> Self {
+    pub fn new(name: &str, server_type: ServerType) -> Self {
         Bot {
             name: name.to_string(),
+            server_type,
             running: false,
+            reaction: HashMap::new(),
+            resp: HashMap::new(),
+            sender: None,
+        }
+    }
+
+    pub fn hear(&mut self, pattern: &str, cb: &'static Callback) {
+        self.reaction
+            .entry(pattern.to_string())
+            .or_insert_with(|| Box::new(cb));
+    }
+
+    pub fn respond(&mut self, pattern: &str, cb: &'static Callback) {
+        self.resp
+            .entry(pattern.to_string())
+            .or_insert_with(|| Box::new(cb));
+    }
+
+    pub fn send(&self, channel: &str, message: &str) {
+        if let Some(tx) = &self.sender {
+            tx.send((
+                channel.to_string(),
+                self.name.to_string(),
+                message.to_string(),
+            ))
+            .expect("send fail");
+        }
+    }
+
+    pub fn reply(&self, channel: &str, nick: &str, message: &str) {
+        if let Some(tx) = &self.sender {
+            tx.send((
+                channel.to_string(),
+                self.name.to_string(),
+                format!("{}: {}", nick, message),
+            ))
+            .expect("send fail");
         }
     }
 
@@ -25,23 +74,39 @@ impl Bot {
         self.running = true;
 
         // https://rust-unofficial.github.io/patterns/idioms/on-stack-dyn-dispatch.html
-        let mut server: Box<dyn Server> = match &self.server {
-            ServerType::Empty => Box::<Empty>::default(),
+        let mut server: Box<dyn Server> = match &self.server_type {
             ServerType::Shell => Box::<Shell>::default(),
         };
 
-        let (tx, rx) = channel::<String>();
+        let (tx, rx) = channel::<(String, String, String)>();
+        self.sender = Some(tx.clone());
         let j_handles = vec![server.connect(tx).unwrap()];
 
-        let mut stdout = stdout();
         while self.running {
-            print!("you> ");
-            stdout.flush().unwrap();
-            let got = rx.recv().unwrap();
+            let (channel, nick, got) = rx.recv().unwrap();
             let message = got.trim();
+            if nick.ne("you") {
+                println!("{}> {}", nick, message);
+            }
+
             if has_shutdown(message) {
                 server.disconnect();
                 self.shutdown();
+            }
+
+            for (pattern, cb) in &self.resp {
+                let pat = format!("{}:? +?{}", self.name, pattern);
+                let re = Regex::from_str(&pat).unwrap();
+                if re.is_match(message) {
+                    cb(self, &channel, &nick, message);
+                }
+            }
+
+            for (pattern, cb) in &self.reaction {
+                let re = Regex::from_str(pattern).unwrap();
+                if re.is_match(message) {
+                    cb(self, &channel, &nick, message);
+                }
             }
         }
         self.finalize(j_handles);
--- src/server.rs
+++ src/server.rs
@@ -1,46 +1,57 @@
 use std::{
-    io,
+    io::{self, stdout, Write},
     sync::{mpsc::Sender, Arc, RwLock},
     thread::{self, JoinHandle},
     time::Duration,
 };
 
 pub trait Server {
-    fn connect(&mut self, tx: Sender<String>) -> Result<JoinHandle<()>, &str>;
+    fn connect(&mut self, tx: Sender<(String, String, String)>) -> Result<JoinHandle<()>, &str>;
     fn disconnect(&self);
 }
 
 #[derive(Debug)]
-pub struct Empty {
+pub struct Shell {
+    tx: Option<Sender<(String, String, String)>>,
     accepted: Option<Arc<RwLock<bool>>>,
 }
 
-impl Empty {
+impl Shell {
     pub fn new() -> Self {
-        Empty { accepted: None }
+        Shell {
+            accepted: None,
+            tx: None,
+        }
     }
 }
 
-impl Default for Empty {
+impl Default for Shell {
     fn default() -> Self {
         Self::new()
     }
 }
 
-impl Server for Empty {
-    fn connect(&mut self, tx: Sender<String>) -> Result<JoinHandle<()>, &str> {
+impl Server for Shell {
+    fn connect(&mut self, tx: Sender<(String, String, String)>) -> Result<JoinHandle<()>, &str> {
         log::trace!("connect");
         let lock0 = Arc::new(RwLock::new(true));
         let lock1 = Arc::clone(&lock0);
         self.accepted = Some(lock0);
+        self.tx = Some(tx.clone());
         let handle = thread::spawn(move || {
             let stdin = io::stdin();
             let dur = Duration::from_millis(10);
+            let mut stdout = stdout();
             while *(lock1.read().expect("acquire read lock fail")) {
                 let mut buf = String::new();
+                print!("you> ");
+                stdout.flush().unwrap();
                 stdin.read_line(&mut buf).expect("read fail");
+                let channel = String::from("empty");
+                let nick = String::from("you");
                 let message = buf.trim();
-                tx.send(message.to_string()).expect("send fail");
+                tx.send((channel, nick, message.to_string()))
+                    .expect("send fail");
                 // sleep 을 주지 않으면 disconnect 에 의해 accepted 값이
                 // 변경되기 전에 loop 로 들어와서 표준입력을 기다림 -> 뭐라도 눌러야 종료되는 상황
                 thread::sleep(dur);
