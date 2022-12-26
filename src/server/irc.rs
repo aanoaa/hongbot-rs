c71f447c3d8a32f3a8eae952101d1bfd71fb25cb
Hyungsuk Hong <aanoaa@gmail.com>
2022-12-26 15:37:12 +0900

    parse nick, servername, hostname

--- src/server/irc.rs
+++ src/server/irc.rs
@@ -32,6 +32,9 @@ enum IrcCommand {
 #[derive(Debug)]
 struct IrcMessage {
     raw: String,
+    nick: Option<String>,
+    servername: Option<String>,
+    hostname: Option<String>,
     command: IrcCommand,
     params: String,
 }
@@ -51,9 +54,20 @@ impl IrcMessage {
         }
 
         let mut s: Vec<&str>;
+        let mut nick = None;
+        let mut servername = None;
+        let mut hostname = None;
         if raw.chars().next().unwrap().eq(&':') {
             let v = raw[1..].split(' ').collect::<Vec<&str>>();
             s = v[1..].to_vec();
+            let from = v[0];
+            if let Some(i) = from.find('!') {
+                nick = Some(from[0..i].to_string());
+                if let Some(j) = from.find('@') {
+                    servername = Some(from[(i + 1)..j].to_string());
+                    hostname = Some(from[(j + 1)..].to_string());
+                };
+            };
         } else {
             s = raw.split(' ').collect::<Vec<&str>>();
         }
@@ -67,11 +81,14 @@ impl IrcMessage {
             "user" => Ok(IrcCommand::USER),
             "privmsg" => Ok(IrcCommand::PRIVMSG),
             "join" => Ok(IrcCommand::JOIN),
-            _ => Err(IrcError::UnknownCommand), // 에러 대신에 ignore 하자
+            _ => Err(IrcError::UnknownCommand),
         }?;
 
         Ok(Self {
             raw: raw.to_string(),
+            nick,
+            servername,
+            hostname,
             command,
             params: s.join(" "),
         })
@@ -102,19 +119,13 @@ impl Server for Irc {
         let lock0 = Arc::new(RwLock::new(false));
         let lock1 = Arc::clone(&lock0);
         self.accepted = Some(lock0);
-
-        // thread 를 나눠야 할 수도?
-        // connection 을 맺고,
-        // read 전용
-        // callback 이 필요할 듯?
-        // join 다음에 할 일 등등
-
         let handle = thread::spawn(move || {
             // https://www.rfc-editor.org/rfc/rfc1459#section-4.1
             // The recommended order for a client to register is as follows:
             // 1. Pass message
             // 2. Nick message
             // 3. User message
+            // + response ping message
             if let Ok(mut stream) = TcpStream::connect(addr) {
                 log::trace!("Connected to the server!");
                 {
@@ -124,7 +135,7 @@ impl Server for Irc {
 
                 let mut s = stream.try_clone().expect("stream clone fail");
                 thread::spawn(move || {
-                    let mut buf = [0; 1024];
+                    let mut buf = [0; 4096];
                     loop {
                         match s.read(&mut buf) {
                             Ok(size) => {
@@ -148,26 +159,21 @@ impl Server for Irc {
                                             s.write_all(command.as_bytes()).unwrap();
                                         }
                                         IrcCommand::PRIVMSG => {
-                                            let channel = String::from("irc");
-                                            let nick = String::from("you");
+                                            let nick = if let Some(nick) = msg.nick {
+                                                nick
+                                            } else {
+                                                "".to_string()
+                                            };
 
-                                            // Parameters: <receiver>{,<receiver>} <text to be sent>
-                                            // :Angel PRIVMSG Wiz :Hello are you receiving this message ?
-                                            //     ; Message from Angel to Wiz.
-                                            //
-                                            // PRIVMSG Angel :yes I'm receiving it !receiving it !'u>(768u+1n) .br
-                                            //     ; Message to Angel.
-                                            //
-                                            // PRIVMSG jto@tolsun.oulu.fi :Hello !
-                                            //     ; Message to a client on server tolsun.oulu.fi with username of "jto".
-                                            //
-                                            // PRIVMSG $*.fi :Server tolsun.oulu.fi rebooting.
-                                            //                                 ; Message to everyone on a server which
-                                            //                                 has a name matching *.fi.
-                                            //
-                                            // PRIVMSG #*.edu :NSFNet is undergoing work, expect interruptions
-                                            //                                 ; Message to all users who come from a
-                                            //                                 host which has a name matching *.edu.
+                                            let s = msg.params.split(' ').collect::<Vec<&str>>();
+                                            if s.len() < 2 {
+                                                // ignore
+                                                log::error!("unexpected message");
+                                                continue;
+                                            }
+
+                                            let channel = String::from(s[0]);
+                                            let message = s[1..].join(" ");
                                             tx.send((channel, nick, message.trim().to_string()))
                                                 .expect("send fail");
                                         }
@@ -190,7 +196,7 @@ impl Server for Irc {
                     }
                 });
 
-                let dur = Duration::from_millis(1000);
+                let dur = Duration::from_millis(5000);
                 thread::sleep(dur);
 
                 let mut cmd = format!("NICK {}{}", nick, CRLF);
@@ -217,6 +223,7 @@ impl Server for Irc {
                 //                                 nickname for which the USER command
                 //                                 belongs to
 
+                // configuration 으로 부터 compose
                 cmd = format!("USER {} * * :hongbot-rs{}", nick, CRLF);
                 match stream.write_all(cmd.as_bytes()) {
                     Ok(()) => {
@@ -230,17 +237,6 @@ impl Server for Irc {
                 thread::sleep(dur);
 
                 cmd = format!("JOIN #foo{}", CRLF);
-                match stream.write_all(cmd.as_bytes()) {
-                    Ok(()) => {
-                        log::trace!("wrote {:?}", &cmd);
-                    }
-                    Err(e) => {
-                        log::error!("wrote {:?} fail: {e}", &cmd);
-                    }
-                }
-
-                thread::sleep(dur);
-                cmd = format!("PRIVMSG aanoaa :Hello{}", CRLF);
                 match stream.write_all(cmd.as_bytes()) {
                     Ok(()) => {
                         log::trace!("wrote {:?}", &cmd);
@@ -263,27 +259,6 @@ impl Server for Irc {
     }
 }
 
-// def parsemsg(s):
-//     """Breaks a message from an IRC server into its prefix, command, and arguments.
-//     """
-//     prefix = ''
-//     trailing = []
-//     if not s:
-//        raise IRCBadMessage("Empty line.")
-//     if s[0] == ':':
-//         prefix, s = s[1:].split(' ', 1)
-//     if s.find(' :') != -1:
-//         s, trailing = s.split(' :', 1)
-//         args = s.split()
-//         args.append(trailing)
-//     else:
-//         args = s.split()
-//     command = args.pop(0)
-//     return prefix, command, args
-
-// parsemsg(":test!~test@test.com PRIVMSG #channel :Hi!")
-// # ('test!~test@test.com', 'PRIVMSG', ['#channel', 'Hi!'])
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -294,10 +269,35 @@ mod tests {
         assert_eq!(msg.raw, ":test!~test@test.com PRIVMSG #channel :Hi!");
         assert_eq!(msg.command, IrcCommand::PRIVMSG);
         assert_eq!(msg.params, "#channel :Hi!");
+        assert_eq!(msg.nick, Some("test".to_string()));
+        assert_eq!(msg.servername, Some("~test".to_string()));
+        assert_eq!(msg.hostname, Some("test.com".to_string()));
 
+        // ping message
         let msg = IrcMessage::from("PING; :ynrYzp}[Bx").unwrap();
         assert_eq!(msg.raw, "PING; :ynrYzp}[Bx");
         assert_eq!(msg.command, IrcCommand::PING);
         assert_eq!(msg.params, ":ynrYzp}[Bx");
+        assert_eq!(msg.nick, None);
+        assert_eq!(msg.servername, None);
+        assert_eq!(msg.hostname, None);
+
+        // direct message from aanoaa -> hongbot
+        let msg = IrcMessage::from(":aanoaa!user@172.21.0.1 PRIVMSG hongbot :bye").unwrap();
+        assert_eq!(msg.raw, ":aanoaa!user@172.21.0.1 PRIVMSG hongbot :bye");
+        assert_eq!(msg.command, IrcCommand::PRIVMSG);
+        assert_eq!(msg.params, "hongbot :bye");
+        assert_eq!(msg.nick, Some("aanoaa".to_string()));
+        assert_eq!(msg.servername, Some("user".to_string()));
+        assert_eq!(msg.hostname, Some("172.21.0.1".to_string()));
+
+        // #foo channel message
+        let msg = IrcMessage::from(":aanoaa!user@172.21.0.1 PRIVMSG #foo :good").unwrap();
+        assert_eq!(msg.raw, ":aanoaa!user@172.21.0.1 PRIVMSG #foo :good");
+        assert_eq!(msg.command, IrcCommand::PRIVMSG);
+        assert_eq!(msg.params, "#foo :good");
+        assert_eq!(msg.nick, Some("aanoaa".to_string()));
+        assert_eq!(msg.servername, Some("user".to_string()));
+        assert_eq!(msg.hostname, Some("172.21.0.1".to_string()));
     }
 }
