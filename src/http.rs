a5f23cc1afda8e6721589f63cd36a0d87dfde133
Hyungsuk Hong <aanoaa@gmail.com>
2023-01-05 11:09:05 +0900

    routing http req and handle each fn

--- src/http.rs
+++ src/http.rs
@@ -1,10 +1,11 @@
 use std::{
+    io::Cursor,
     sync::Arc,
     thread::{self, JoinHandle},
 };
 
 use anyhow::Result;
-use tiny_http::{Response, Server};
+use tiny_http::{Request, Response, Server, StatusCode};
 
 pub fn serve(addr: &str) -> Result<(Arc<Server>, Vec<JoinHandle<()>>)> {
     let server = Arc::new(Server::http(addr).unwrap());
@@ -15,7 +16,15 @@ pub fn serve(addr: &str) -> Result<(Arc<Server>, Vec<JoinHandle<()>>)> {
         let server = server.clone();
         let guard = thread::spawn(move || {
             for req in server.incoming_requests() {
-                let resp = Response::from_string("OK");
+                let path = req.url();
+                let method = req.method();
+                let resp = match method {
+                    tiny_http::Method::Get => match path {
+                        "/" => index(&req),
+                        _ => error_resp(404),
+                    },
+                    _ => error_resp(405),
+                };
                 req.respond(resp).unwrap();
             }
         });
@@ -23,4 +32,15 @@ pub fn serve(addr: &str) -> Result<(Arc<Server>, Vec<JoinHandle<()>>)> {
     }
 
     Ok((server, guards))
+}
+
+fn index(req: &Request) -> Response<Cursor<Vec<u8>>> {
+    log::trace!("{} {}", req.method(), req.url());
+    // do something
+    Response::from_string("OK")
+}
+
+fn error_resp(code: u16) -> Response<Cursor<Vec<u8>>> {
+    let code = StatusCode(code);
+    Response::from_string(code.default_reason_phrase()).with_status_code(code)
 }
